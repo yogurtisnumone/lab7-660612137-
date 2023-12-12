49c5eb665dd317079e65ae500d8e8f5980918c3d
kachapat_p <kachapat_p@cmu.ac.th>
2023-12-13 02:10:57 +0700

    lab3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if(x < 'A' || x > 'Z') return '0';
+    if(x == 'A') return 'Z';
+    return x - 1;
 }
 
 int main(){
